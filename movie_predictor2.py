9bad432f7d7cd8ec3203698f21baa5f6a10c227f
William Bowditch <bowditcw@bc.edu>
2016-12-05 21:54:58 -0500

    cross fold

--- movie_predictor2.py
+++ movie_predictor2.py
@@ -1,3 +1,10 @@
+from sklearn import svm
+import pandas as pd
+import optunity
+import optunity.metrics
+import matplotlib.pyplot as pyplot
+from sklearn.model_selection import cross_val_score
+
 data_table = pd.read_csv("total_set.csv",index_col=0)
 film_titles = data_table.index  # list of all of our movie titles in the dataset.
 #print film_titles
@@ -22,22 +29,26 @@ data = data_table[['Sequel',
 data_norm = (data - data.mean()) / (data.max() - data.min())
 
 profitability_target = data_table['Profitable']
-gross_target = data_table['Domestic Gross']
-tomato = data_table['Rotten']
+#gross_target = data_table['Domestic Gross']
+#tomato = data_table['Rotten']
 
 
-normalized_target_gross = (gross_target - gross_target.mean()) / (gross_target.max() - gross_target.min())
+#normalized_target_gross = (gross_target - gross_target.mean()) / (gross_target.max() - gross_target.min())
 #tomato = (tomato - tomato.mean()) / (tomato.max() - tomato.min())
 
 
 clf_profit = svm.SVC(kernel='linear',C=1000,verbose=True)
 clf_profit.fit(data_norm,profitability_target)
+scores = cross_val_score(clf_profit, data_norm, profitability_target, cv=20)
 
-clf_rating = svm.SVR(C=1000)
-clf_rating.fit(data_norm,normalized_target_gross)
-
-clf_tomato = svm.SVR()
-clf_tomato.fit(data_norm,tomato)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
-print clf_profit.coef_
+# clf_rating = svm.SVR(C=1000)
+# clf_rating.fit(data_norm,normalized_target_gross)
+
+# clf_tomato = svm.SVR()
+# clf_tomato.fit(data_norm,tomato)
+
+
+#print clf_profit.coef_
